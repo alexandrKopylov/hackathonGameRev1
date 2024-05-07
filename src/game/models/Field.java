1bc10fb682ab53b313b178cf8d81b7ccd6cac45f
Sasha <sash-2007@ya.ru>
2024-05-07 17:48:22 +0500

    urok 34

--- src/game/models/Field.java
+++ src/game/models/Field.java
@@ -15,6 +15,17 @@ public class Field {
         setStateField(DEFAULT_SYMBOL);
     }
 
+    private boolean checkCoordinate (int coordinate){
+        if(coordinate >= getMIN_NUMBER() && coordinate <=getMAX_NUMBER()){
+            return true;
+
+        }else{
+            System.out.println("Проверте координаты");
+            return false;
+        }
+    }
+
+
     public void setStateField(String symbol) {
         for (int i = 0; i < getSIZE_FIELD(); i++) {
             for (int j = 0; j < getSIZE_FIELD(); j++) {
@@ -25,8 +36,25 @@ public class Field {
     }
 
     public void setCellFild(int i, int j, String symbol) {
-        this.stateField[i][j]= symbol;
+        if(checkCoordinate(i) && checkCoordinate(j)){
+           if(stateField[i][j] == DEFAULT_SYMBOL || stateField[i][j] == null ){
+               this.stateField[i][j]= symbol;
+           }else{
+               System.out.println("Поле занято");
+           }
+
+        }
+
     }
+
+    public String getCellField (int i , int j){
+        if(checkCoordinate(i) && checkCoordinate(j)) {
+            return stateField[i][j];
+        }
+        return null;
+    }
+
+
 
     public int getSIZE_FIELD() {
         return SIZE_FIELD;
